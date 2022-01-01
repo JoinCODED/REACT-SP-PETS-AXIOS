bfe86c9eae8259e7591a7309f3fd211daf9fff82
YanalShmilan <84308096+YanalShmilan@users.noreply.github.com>
2022-01-01 19:00:09 +0200

    Update petStore.js

--- src/petStore.js
+++ src/petStore.js
@@ -1,7 +1,7 @@
 import { action, makeObservable, observable } from 'mobx';
-import petsData from './petsData';
+import axios from 'axios';
 class PetStore {
-  pets = petsData;
+  pets = [];
 
   constructor() {
     makeObservable(this, {
@@ -12,21 +12,48 @@ class PetStore {
     });
   }
 
-  handleAdopt = (petId) => {
-    this.pets = this.pets.filter((pet) => pet.id !== petId);
+  fetchPets = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/pets');
+      this.pets = response.data;
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  addPet = (pet) => {
-    pet.id = this.pets[this.pets.length - 1].id + 1;
-    this.pets.push(pet);
+  handleAdopt = async (petId) => {
+    try {
+      await axios.delete(`http://localhost:5000/pets/${petId}`);
+      this.pets = this.pets.filter((pet) => pet.id !== petId);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  updatePet = (updatedPet) => {
-    this.pets = this.pets.map((pet) =>
-      pet.id === updatedPet.id ? updatedPet : pet
-    );
+  addPet = async (pet) => {
+    try {
+      const response = await axios.post('http://localhost:5000/pets', pet);
+      this.pets.push(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  updatePet = async (updatedPet) => {
+    try {
+      const response = await axios.put(
+        `http://localhost:5000/pets/${updatedPet.id}`,
+        updatedPet
+      );
+      this.pets = this.pets.map((pet) =>
+        pet.id === updatedPet.id ? response.data : pet
+      );
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
 
 const petStore = new PetStore();
+petStore.fetchPets();
 export default petStore;
